9d4ab67a9ab16cde23c7534e52840fc344d9e325
Adityacodin <mohite.aditya.comp@kgce.edu.in>
2024-03-19 17:10:37 +0530

    work logic complete

--- Calc.py
+++ Calc.py
@@ -16,18 +16,29 @@ class App(ctk.CTk):
         # per = 0
         # lumpSum = 0
 
-#  last
-        # def getAns(amt,te,pe) :
-        #     times = int(te*12)
-        #     for i in range(1,times+1) :
+#
 
+
+        def Display(self,M,N):
+            self.MatDisp.configure(text=str(format(M,',d')))
+            self.AmtDisp.configure(text=str(format(N,',d')))
+
+
+        def getAns(self,amt,te,pe) :
+            P = amt
+            i = float(pe/12)
+            n = te*12
+
+            M = int(P * ((pow((1+i),n)-1)/i) * (1+i))
+            N = int(amt*n)
+            print(M,N)
+            Display(self,M,N)
             
 
         def pressedCalculate():
             global amount
             if self.Amt.get() != '':
                 amount = int(self.Amt.get())
-                print(amount)
             elif self.ls.get() != '':
                 amount = int(self.ls.get())
 
@@ -36,8 +47,8 @@ class App(ctk.CTk):
 
             global per
             per = float(int(self.roi.get())/100)
-            print(per)
-            getAns(amount,tenure,per)
+
+            getAns(self,amount,tenure,per)
 
             
         # print(amount)
@@ -46,6 +57,7 @@ class App(ctk.CTk):
             # self.ten.delete(0,'end')
             self.ls.delete(0,'end')
             self.roi.delete(0,'end')
+            
 
 
         self.title("SIP Calculator")
@@ -86,13 +98,13 @@ class App(ctk.CTk):
         self.InvAmt = ctk.CTkLabel(self,text ="Invested Amount")
         self.InvAmt.grid(row = 5, column = 0, padx = 20, pady = 20, sticky = 'ew')
 
-        self.AmtDisp = ctk.CTkTextbox(self,width = 10,height = 20)
+        self.AmtDisp = ctk.CTkLabel(self,text='')
         self.AmtDisp.grid(row = 5, column = 1, padx=20, pady = 20, sticky = 'ew')
 
         self.MatVal = ctk.CTkLabel(self,text='Maturity Value   ')
         self.MatVal.grid(row = 6, column = 0,padx = 10,pady = 20, sticky = 'ew' )
 
-        self.MatDisp = ctk.CTkTextbox(self,width = 10, height=20)
+        self.MatDisp = ctk.CTkLabel(self,text='')
         self.MatDisp.grid(row = 6, column = 1, padx = 20,pady =20, sticky = 'ew')
 
         
