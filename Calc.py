ad002583c668a151ccbef63d759cd8c3d6fb4d78
Adityacodin <mohite.aditya.comp@kgce.edu.in>
2024-03-29 18:14:43 +0530

    frame add and size change

--- Calc.py
+++ Calc.py
@@ -4,8 +4,8 @@ import math
 import matplotlib.pyplot as plt 
 import numpy as np
 
-ctk.set_appearance_mode("dark")
-ctk.set_default_color_theme("green")
+ctk.set_appearance_mode("bright")
+ctk.set_default_color_theme("blue")
 
 class App(ctk.CTk):
     
@@ -25,8 +25,8 @@ class App(ctk.CTk):
         def Display(self,M,N):
             self.MatDisp.configure(text=str(format(M,',d')))
             self.AmtDisp.configure(text=str(format(N,',d')))
-            self.Details = ctk.CTkButton(self,text='Details',bg_color = '#02b165',border_color='#02b165',command = lambda: dispPie(self,M,N))
-            self.Details.grid(row = 4, column = 2, padx = 20, pady = 20, sticky= 'ew')
+            self.Details = ctk.CTkButton(self,text='Details',fg_color = '#E74C3C',border_color='#E74C3C',command = lambda: dispPie(self,M,N))
+            self.Details.grid(row = 6, column = 0, padx = 20, pady = 20, sticky= 'ew')
             # dispPie(self,M,N)
 
         
@@ -80,53 +80,57 @@ class App(ctk.CTk):
 
 
         self.title("SIP Calculator")
-        self.geometry('600x600')
+        self.geometry('330x600')
 
-        self.SIPamt = ctk.CTkLabel(self,text = 'SIP Amount ::::  ')
+        self.InputFrame = ctk.CTkFrame(master = self,fg_color='#F5B7B1')
+        self.InputFrame.grid(row =0,column=0,sticky='ew')
+
+        self.SIPamt = ctk.CTkLabel(master=self.InputFrame,text = 'SIP Amount  ')
         self.SIPamt.grid(row = 0, column = 0, padx = 20, pady = 20, sticky = 'ew')
 
-        self.Amt = ctk.CTkEntry(self,placeholder_text = '2000',border_color="#02b165")
+        self.Amt = ctk.CTkEntry(master=self.InputFrame,placeholder_text = '2000',border_color="#E74C3C")
         self.Amt.grid(row = 0, column = 1, padx = 20, pady = 20, sticky = 'ew')
         
 
-        self.Tenure = ctk.CTkLabel(self, text = "Tenure (in years) ::::  ")
+        self.Tenure = ctk.CTkLabel(master=self.InputFrame, text = "Tenure (in years) ")
         self.Tenure.grid(row = 1, column = 0, padx = 20, pady = 20,sticky = 'ew')
 
-        self.ten = ctk.CTkComboBox(self,values = ['5','10','15','20','25','30','35'],border_color="#02b165",button_color="#02b165",dropdown_hover_color="#02b165")
+        self.ten = ctk.CTkComboBox(master=self.InputFrame,values = ['5','10','15','20','25','30','35'],border_color="#E74C3C",button_color="#E74C3C",dropdown_hover_color="#02b165")
         self.ten.grid(row = 1, column = 1, padx = 20, pady = 20, sticky = 'ew')
 
-        self.ror = ctk.CTkLabel(self, text = 'Rate of Return (%) ::::  ')
+        self.ror = ctk.CTkLabel(master=self.InputFrame, text = 'Rate of Return (%) ')
         self.ror.grid(row = 2, column = 0, padx = 20, pady = 20,sticky = 'ew')
 
-        self.roi = ctk.CTkEntry(self, placeholder_text= '12%',border_color="#02b165")
+        self.roi = ctk.CTkEntry(master=self.InputFrame, placeholder_text= '12%',border_color="#E74C3C")
         self.roi.grid(row = 2, column = 1, padx = 20, pady = 20, sticky = 'ew')
 
 
-        self.lumpSum = ctk.CTkLabel(self,text='Lump-sum Amount ::::  ')
+        self.lumpSum = ctk.CTkLabel(master=self.InputFrame,text='Lump-sum Amount  ')
         self.lumpSum.grid(row = 3, column = 0, padx = 20, pady = 20, sticky = 'ew')
 
-        self.ls = ctk.CTkEntry(self, placeholder_text = '1,00,000',border_color="#02b165")
+        self.ls = ctk.CTkEntry(master=self.InputFrame, placeholder_text = '1,00,000',border_color="#E74C3C")
         self.ls.grid(row = 3, column = 1, padx = 20, pady = 20, sticky = 'ew')
 
-        self.Calculate = ctk.CTkButton(self,text="Calculate",bg_color="#02b165",border_color="black", command = pressedCalculate)
-        self.Calculate.grid(row = 4, column = 0, padx = 20, pady = 20 ,sticky = 'ew')
+        self.Output = ctk.CTkFrame(master=self,fg_color='#E74C3C')
+        self.Output.grid(row=1,column=0,sticky='ew')
 
-        self.Reset = ctk.CTkButton(self,text="Reset",bg_color="#02b165",border_color="black",command = pressedReset)
-        self.Reset.grid(row = 4,column = 1, padx =20, pady = 20, sticky = 'ew')
-
-
-        self.InvAmt = ctk.CTkLabel(self,text ="Invested Amount")
+        self.InvAmt = ctk.CTkLabel(master=self.Output,text ="Invested Amount")
         self.InvAmt.grid(row = 5, column = 0, padx = 20, pady = 20, sticky = 'ew')
 
-        self.AmtDisp = ctk.CTkLabel(self,text='')
+        self.AmtDisp = ctk.CTkLabel(master=self.Output,text='')
         self.AmtDisp.grid(row = 5, column = 1, padx=20, pady = 20, sticky = 'ew')
 
-        self.MatVal = ctk.CTkLabel(self,text='Maturity Value   ')
-        self.MatVal.grid(row = 6, column = 0,padx = 10,pady = 20, sticky = 'ew' )
+        self.MatVal = ctk.CTkLabel(master=self.Output,text='Maturity Value   ')
+        self.MatVal.grid(row = 6, column = 0,padx = 10,pady = 20, sticky = 'ew')
 
-        self.MatDisp = ctk.CTkLabel(self,text='')
+        self.MatDisp = ctk.CTkLabel(master=self.Output,text='')
         self.MatDisp.grid(row = 6, column = 1, padx = 20,pady =20, sticky = 'ew')
+        
+        self.Calculate = ctk.CTkButton(self,text="Calculate",fg_color="#E74C3C",border_color="black", command = pressedCalculate)
+        self.Calculate.grid(row = 4, column = 0, padx = 20, pady = 20 ,sticky = 'ew')
 
+        self.Reset = ctk.CTkButton(self,text="Reset",fg_color="#E74C3C",border_color="black",command = pressedReset)
+        self.Reset.grid(row = 5,column = 0, padx =20, pady = 20, sticky = 'ew')
         
 
 if __name__=="__main__":
